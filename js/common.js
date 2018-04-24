0b3baa4e3e6b9488e600fb98c01d62014410e8e1
DamirAid <37047822+DamirAid@users.noreply.github.com>
2018-04-24 19:30:38 +0600

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+	
+	$.stellar({
+	responsive: true,
+	horizontalOffset: 60
+});
+
+$('.owl-carousel').owlCarousel({
+      loop:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true   
+        }
+    },
+    navText : ""
+});
 
 
 $(".top_phone .wrapper .tab").click(function() {
